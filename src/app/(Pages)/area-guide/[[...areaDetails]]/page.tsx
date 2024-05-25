c521b930170ff7da934f9685dcd7e7ecd7f5fd89
Keegan Bruer <keegan.bruer@gmail.com>
2024-05-25 01:31:00 -0400

    area guide generative titles

--- src/app/(Pages)/area-guide/[[...areaDetails]]/page.tsx
+++ src/app/(Pages)/area-guide/[[...areaDetails]]/page.tsx
@@ -1,13 +1,32 @@
 import Head from "next/head";
 import Image from "next/image";
 import Content from "./content";
-import { Metadata } from "next";
+import { Metadata, ResolvingMetadata } from "next";
 import { geo_data } from "./(Map)/GeoData";
-
 import { BuildPageMeta } from "@/app/metaDefaults";
-export const metadata: Metadata = BuildPageMeta({
-  title:"Pinehurst Lodge - Local Area Guide"
-})
+
+type Props = {
+  params: { areaDetails: string[] }
+  searchParams: { [key: string]: string | string[] | undefined }
+}
+export const generateMetadata = async (
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> => {
+  let title = "Local Area Guide - Pinehurst Lodge";
+  
+  if (params.areaDetails != undefined) {
+    let pageDetails =  geo_data.features.filter((feature) => {
+      return feature.properties.id.join("/") == params.areaDetails.join("/");
+    })[0]
+    if (pageDetails != undefined) {
+      title = `${pageDetails.properties.name} - ${title}`
+    }
+  }
+  return BuildPageMeta({
+    title
+  })
+}
 
 export function generateStaticParams() {
   let routes =  geo_data.features.map((feature) => {

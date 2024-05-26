c4bb23e217b9d1cbc5a03db67a44ec35c0988729
Keegan Bruer <keegan.bruer@gmail.com>
2024-05-25 22:17:59 -0400

    Local Activities Section

--- src/app/(Pages)/(Home)/(Sections)/LocalActivities.tsx
+++ src/app/(Pages)/(Home)/(Sections)/LocalActivities.tsx
@@ -10,6 +10,7 @@ import { FaRegCalendarPlus } from "react-icons/fa";
 import { MdOutlineOpenInNew } from "react-icons/md";
 import { PageFooter, AlternateSection, Section, responsiveMobileWidth } from "@/app/globalStyles";
 import Link from "next/link";
+import { BsBoxArrowUpRight } from "react-icons/bs";
 
 export default () => {
   return (
@@ -18,17 +19,62 @@ export default () => {
         <h1 className="heading">Plenty of Local Activities</h1>
         <CardWrapper>
           <div className="Card">
-            
+            <TwoColumns data-reverse="true">
+              <TwoImages>
+                <img src="/AreaAttractions/Toss_Pizza.jpg"></img>
+                <img src="/AreaAttractions/Toss_Pizza_exterior.png"></img>
+              </TwoImages>
+              <CenterColumn>
+                <h3>Toss Pizza </h3>
+                <p>Within walking distance on Main Street in Rollinsville is the best pizza in the surrounding area, Toss Wood Fired Pizza. Featuring fresh ingredients, salads, small plate veggies, local beverages and more. It’s a dining delight not to be missed and a gift to be so nearby!</p>
+                <Link href="https://www.tosswoodfired.com/" target="_blank">View The Toss Website <BsBoxArrowUpRight /></Link>
+              </CenterColumn>
+            </TwoColumns>
           </div>
           <div className="Card">
-
+            <TwoColumns>
+              <TwoImages>
+                <img src="/AreaAttractions/Redrocks.jpg"></img>
+                <img src="/AreaAttractions/Red-Rocks-Amphitheatre.jpeg"></img>
+              </TwoImages>
+              <CenterColumn>
+                <h3>Redrocks </h3>
+                <p>
+                </p>
+                <Link href="#" target="_blank">Visit Redrocks Online <BsBoxArrowUpRight /></Link>
+              </CenterColumn>
+            </TwoColumns>
           </div>
           <div className="Card">
-
+            <TwoColumns data-reverse="true">
+              <TwoImages>
+                <img src="/AreaAttractions/GoldDirtDistillery3.jpg"></img>
+                <img src="/AreaAttractions/GoldDirtBuilding.webp"></img>
+              </TwoImages>
+              <CenterColumn>
+                <h3>Gold Dirt Distillery </h3>
+                <p>
+                  Located just down the street from Pinehurst Lodge sits one of the finest distilleries of quality craft spirits. Open Wed - Sun, they offer a delicious variety of hand crafted cocktails and an array of spirits including Rose Gin, Lavender Infused Gin, Honey Spirit, High & Dry Gin and more.
+                </p>
+                <Link href="#" target="_blank">Explore Gold Dirt's Website<BsBoxArrowUpRight /></Link>
+              </CenterColumn>
+            </TwoColumns>
           </div>
           <div className="Card">
-
+            <TwoColumns>
+              <TwoImages>
+                <img src="/HousePhotos/downstairs_living/downstairs_living_room_6.jpg"></img>
+                <img src="/HousePhotos/downstairs_living/downstairs_living_room_6.jpg"></img>
+              </TwoImages>
+              <CenterColumn>
+                <h3>Rocky Mountain National Park </h3>
+                <p>
+                </p>
+                <Link href="#" target="_blank">Checkout The Park's Website  <BsBoxArrowUpRight /></Link>
+              </CenterColumn>
+            </TwoColumns>
           </div>
+         
         </CardWrapper>
       </AlternateSection>
     </>
@@ -46,8 +92,96 @@ const CardWrapper = styled.div`
   .Card {
     border-radius: 10px;
     width: 100%;
-    height: 100px;
     background-color: var(--theme-color-1);
   }
 
+`
+
+const TwoColumns = styled.div`
+  display: flex;
+  flex-direction: row;
+  &[data-reverse="true"] {
+    flex-direction: row-reverse;
+  }
+  @media screen and (max-width: ${()=>responsiveMobileWidth}) {
+    &, &[data-reverse="true"] {
+      flex-direction: column;
+    }
+  }
+  img {
+    width: 100px;
+    aspect-ratio: 3 / 2;
+  }
+  div {
+    flex: 1;
+  }
+`
+
+const CenterColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0px 20px;
+
+  h3 {
+    color: black;
+    font-size: 20px;
+    margin-bottom: 0px;
+    @media screen and (max-width: ${()=>responsiveMobileWidth}) {
+      & {
+        margin-top: 0px;
+      }
+    }
+  }
+  p {
+    color: black;
+    font-size: 18px;
+  }
+  a {
+    width: 80%;
+    padding: 10px;
+    box-sizing: border-box;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: white;
+    background-color: var(--theme-color-3);
+    text-decoration: none;
+    margin: 10px 0px;
+    gap: 10px;
+    svg {
+      width: 20px;
+      height: 20px;
+      color: white;
+    }
+  }
+`
+const TwoImages = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  box-sizing: border-box;
+  padding: 20px;
+  gap: 10px;
+  
+  img {
+    border-radius: 6px;
+    flex: 1;
+    object-fit: cover;
+    --width: 180px;
+    --height: 180px;
+    @media screen and (max-width: ${()=>responsiveMobileWidth}) {
+      & {
+        --width: 80px;
+        --height: 80px;
+      }
+    }
+    width: var(--width);
+    min-width: var(--width);
+    max-width: var(--width);
+    height: var(--height);
+    min-height: var(--height);
+    max-height: var(--height);
+  }
 `

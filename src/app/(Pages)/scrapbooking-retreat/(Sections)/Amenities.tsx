9a24eadf3b4734524a166832a7f00f0c9474ee33
Keegan Bruer <keegan.bruer@gmail.com>
2024-06-28 23:27:24 -0400

    Meta work

--- src/app/(Pages)/scrapbooking-retreat/(Sections)/Amenities.tsx
+++ src/app/(Pages)/scrapbooking-retreat/(Sections)/Amenities.tsx
@@ -54,7 +54,7 @@ export default () => {
             
           </div>
         </TwoColumns>
-        <MoreLink href="/accommodations">View All Accommodations & Amenities <br/> For Your Srapbooking Retreat</MoreLink>
+        <MoreLink href="/accommodations">View All Accommodations & Amenities <br/> For Your Scrapbooking Retreat</MoreLink>
       </Section>
     </>
   );
